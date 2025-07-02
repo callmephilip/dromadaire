85453978f7e084a028d3adb5b135ca9aff33edd2
Philip Nuzhnyi <philip.nuzhnyy@gmail.com>
2025-07-02 15:22:30 +0100

    topbar

--- src/dromadaire/app.py
+++ src/dromadaire/app.py
@@ -1,19 +1,21 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Footer, Header, Static
+from textual.widgets import Footer, Static
 from rich_pixels import Pixels
 from rich.console import Console
+from .topbar import TopBar
 
 class DromadaireApp(App):
     """A Textual app to manage stopwatches."""
 
     BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
     TITLE = "Dromadairez"
+    CSS_PATH = "app.tcss"
 
     def compose(self) -> ComposeResult:
         """Create child widgets for the app."""
         logo_pixels = Pixels.from_image_path("assets/dromadaire.png", resize=(20, 20))
+        yield TopBar()
         yield Static(logo_pixels, id="logo")
-        yield Header()
         yield Footer()
 
     def action_toggle_dark(self) -> None:
